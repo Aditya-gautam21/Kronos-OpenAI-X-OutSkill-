0825318a05e7935212ffbf410155a03dc6aec23f
Aditya-gautam21 <adigaur121@gmail.com>
2026-05-27 11:23:32 +0530

    feat: added logging to remove un-necessary output logs

--- backend/local_llm.py
+++ backend/local_llm.py
@@ -1,8 +1,11 @@
 import os
+import logging
 from dotenv import load_dotenv
 from llama_cpp import Llama
 
 load_dotenv()
+
+logging.getLogger("llama_cpp").setLevel(logging.CRITICAL)
 
 _llm = None
 
@@ -11,10 +14,10 @@ def get_llm():
     if _llm is None:
         _llm = Llama(
             model_path=os.getenv("LOCAL_MODEL_PATH"),
-            n_gpu_layers=21,
-            n_ctx=2048,
+            n_gpu_layers=24,
+            n_ctx=3072,
             n_threads=4,
             use_mlock=True,
-            verbose=False
+            verbose=False,
         )
     return _llm
